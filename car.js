a4a71f6cb018dec801ee7e06fd8553154d3d4e24
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-02-05 13:57:15 +0000

    create functionality CRUD for car object

--- car.js
+++ car.js
@@ -1,3 +1,43 @@
 `use strict`
 
-// fetching for a car
+'use strict';
+
+// C - Create - Fetch POST (CAR CREATE)
+
+const cName = document.querySelector("#name");
+const cMake = document.querySelector("#make");
+const cModel = document.querySelector("#model");
+const cDoors = document.querySelector("#doors");
+const cColour = document.querySelector("#colour")
+
+const postToCar = ()=> {
+    const ownerName = cName.value;
+    const carMake = cMake.value;
+    const carModel = cModel.value;
+    const carDoors = cDoors.value;
+    const carColour = cColour.value;
+
+let data = {
+    name:ownerName,
+    make:carMake,
+    model:carModel,
+    doors:carDoors,
+    colour:carColour,
+    garage:{
+        id:1
+    }
+}
+console.log(data)
+
+fetch("http:localhost:9092/car/create",{
+    method:`POST`,
+    headers:{
+        "Content-type":"application/json"
+    },
+    body: JSON.stringify(data)
+})
+.then((res)=>res.json())
+.then((d)=>console.info(`Request was all good with json response ${d}`))
+.catch((err)=> console.error(err));
+
+}
